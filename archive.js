f07e0e59e952a0ba6b06605608c67dfb671cbd90
dragonofdeseret <daodezion@icloud.com>
2026-04-04 07:57:20 -0600

    sytn

--- archive.js
+++ archive.js
@@ -1346,7 +1346,7 @@ const archive = normalizeArchive([
     year: 2024,
     date: "2024-01-09",
     file: "pdf/ND.pdf",
-    sections: ["trips"]
+    sections: ["trips"],
     substance: ["Psilocybin", "Visionary"]
   },
   {
