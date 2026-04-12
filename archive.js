a023015a551056a77a6748c0c075c18f38af0bf4
dragonofdeseret <daodezion@icloud.com>
2026-04-11 21:48:12 -0600

    tag

--- archive.js
+++ archive.js
@@ -2665,6 +2665,7 @@ const archive = normalizeArchive([
     year: 2022,
     file: "pdf/HDatBoB.pdf",
     sections: ["trips"],
+    substance: ["DMT],
     tags: ["dmt", "visionary", "consciousness", "phenomenology", "mormon-theology", "metaphysics"]
   },
 {
